beaffe658d47122822c232dd6f85bca74e453ae7
miaortizma <maortizm99@gmail.com>
2016-09-13 10:35:27 -0500

    Create ContactsBook.java

---
+++ business/ContactsBook.java
@@ -0,0 +1,25 @@
+package business;
+public class ContactsBook
+{
+  public static void addContact(){
+  
+  }
+  
+  public static void removeContact(){
+  
+  
+  }
+  
+  public static void updateContact(){
+  
+  
+  }
+  
+  public static void printAllContacts(){}
+  
+  public static void printContactsKeys(){}
+  
+  public static void main(String[] args)
+  {
+  }
+}
